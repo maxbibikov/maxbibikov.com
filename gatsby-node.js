5536e5c7e9783eabbfdf56710a76a0918389ecd2
Maksym Bibikov <bibikovmax@gmail.com>
2020-12-16 11:56:31 +0200

    refactor: use new route api
    Use new gatsby's file system route API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,49 +1,4 @@
-const path = require("path");
 const { createFilePath } = require("gatsby-source-filesystem");
-
-exports.createPages = async ({ actions, graphql }) => {
-  const { createPage } = actions;
-
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            id
-            fields {
-              slug
-            }
-            frontmatter {
-              templateKey
-              title
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  if (result.errors) {
-    throw result.errors;
-  }
-
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    const id = node.id;
-
-    createPage({
-      path: `${node.frontmatter.templateKey}${node.fields.slug}`,
-      component: path.resolve(
-        `src/templates/${String(node.frontmatter.templateKey)}.js`
-      ),
-      context: {
-        id,
-      }, // additional data can be passed via context
-    });
-  });
-};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
